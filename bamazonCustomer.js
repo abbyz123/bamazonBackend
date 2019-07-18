df2412a9eaa38b739e810bbf68b2091651c9a51d
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-07-17 22:28:10 -0700

    bamazon customer app is done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,11 +6,12 @@ let db = require("./mysql.js");
 //console.log(db.login)
 
 // start mysql connection
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+let mysql = require("mysql");
+let inquirer = require("inquirer");
+let promise = require("promise");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
     // host name
     host: db.login.host,
 
@@ -40,10 +41,13 @@ async function start() {
 
     // Show all the items
     connection.query("SELECT * from products", function (error, results, fields) {
+        if (error) throw error;
+        
         console.log("Available products: ");
-        console.log("Item ID\tProduct Name\tPrice ($)");
+        console.log("Item ID\tProduct Name\tPrice ($)\tQuantity");
+
         results.forEach(rawdata => {
-            console.log(rawdata.item_id + '\t' + rawdata.product_name + '\t' + rawdata.price);
+            console.log(rawdata.item_id + '\t' + rawdata.product_name + '\t' + rawdata.price  + '\t' + rawdata.stock_quantity);
         });
 
         // input prompt
@@ -51,19 +55,45 @@ async function start() {
             {
                 name: "id",
                 type: "input",
-                message: "Please enter the item id to purchase: "
+                message: "Please enter the item id to purchase:"
             },
             {
                 name: "quantity",
                 type: "input",
-                message: "Please enter the quantity of the purchasing item"
+                message: "Please enter the quantity of the purchasing item:"
             }
         ]).then(function (answer) {
-            console.log(answer.id);
-            console.log(answer.quantity);
+            idQuery = "SELECT * from products where item_id = " + mysql.escape(answer.id);
+            connection.query(idQuery, function(error, results, fields) {
+                if (error) throw error;
+                // end the process if the item does not exist
+                if (0 === results.length) {
+                    console.log("item is not found.");
+                    connection.end();
+                    process.exit(1);
+                }
+
+                // end the process if the request quantity exceeds the in stock amount
+                let quantity = parseInt(answer.quantity);
+                if (quantity > results[0].stock_quantity) {
+                    console.log("Insufficient quantity.")
+                    connection.end();
+                    process.exit(1);
+                }
+
+                // new stock quanity and the total cost
+                let newQuantity = results[0].stock_quantity - quantity;
+                let totalCost = Number(quantity * results[0].price).toFixed(2);     // show only two decimal
+                
+                // update db query
+                connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newQuantity, answer.id], function(error, results, fields) {
+                    if (error) throw error;
+                    console.log("Thank you. Your order has been placed.");
+                    console.log("Your total cost is: " + totalCost + " dollars");
+                    connection.end();
+                })
+            });
         })
     });
-
-    connection.end();
 }
 
